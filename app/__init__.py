63e70bd3fc0b47d0e0232ae3243760d405f89c4e
eddy0 <awindy369@gmail.com>
2020-07-08 21:29:53 -0700

    init

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_migrate import Migrate
+
+from app.models import db
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+def main():
+    from app.views import app as views
+    app.register_blueprint(views)
+    app.run(host='0.0.0.0', debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>'.format(self.body)
